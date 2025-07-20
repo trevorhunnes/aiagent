1f2a66fd3a6d6a9423e4ef809098106962875525
harimang <thunnes@hotmail.com>
2025-07-20 08:05:31 -0700

    added command line prompt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
 
@@ -8,9 +9,15 @@ client = genai.Client(api_key=api_key)
 
 
 def main():
+    try:
+        prompt = sys.argv[1]
+    except Exception:
+        print("Please enter a promt")
+        sys.exit(1)
+
     response = client.models.generate_content(
         model='gemini-2.0-flash-001',
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=prompt
     )
 
     print(response.text)
